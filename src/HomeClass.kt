ba12d462800dc35dc5846b5d01cc8026e8727871
Abhinav Sharma <abhinav.ih@gmail.com>
2017-10-24 23:04:28 +0530

    few demo methods on looping and control flow

--- src/HomeClass.kt
+++ src/HomeClass.kt
@@ -9,9 +9,66 @@
  * */
 
 class HomeClass {
+
+
     companion object {
-        @JvmStatic fun main(args: Array<String>){
-            print("Hello World")
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("Hello World")
+            println("Greater number out of 4 and 7 is ${findGreaterNumber(4, 7)}")
+            findSum(y = null)
+            findSum(y = 4)
+            findSum(5, 6)
+            getGradeFromAge(3)
+            printPattern()
+            println("Factorial of 5 is ${getFactorial(5)}")
         }
     }
+
+
+}
+
+/**
+ * Inline function demo
+ */
+fun findGreaterNumber(x: Int, y: Int) = if (x > y) x else y
+
+/**
+ * Default value demo
+ * Null safety demo
+ * */
+fun findSum(x: Int = 2, y: Int?) {
+    if (y != null) println(x + y) else println("Null cant be added")
+}
+
+/**
+ * when demo
+ * */
+fun getGradeFromAge(age: Int) {
+    when (age) {
+        in 0..5 -> println("Enjoy")
+        5, 6, 7, 8 -> println("go to small")
+        else -> println("go to large")
+    }
+}
+
+/**
+ * for loop
+ * */
+fun printPattern() {
+    for (i in 1..5) {
+        for (j in 1..i)
+            print("*")
+        println()
+    }
+}
+
+/**
+ * Factorial demo
+ * */
+fun getFactorial(i: Int) : Int{
+    var fact = 1
+    for (j in 1..i)
+        fact *= j
+    return fact
 }
